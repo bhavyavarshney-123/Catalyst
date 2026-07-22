a77815af190a55d35281a4fb308f1d001f346107
bhavyavarshney-123 <bhavya.bhavya.varshney@gmail.com>
2026-07-22 23:33:09 +0530

    implemented nodes for the agent

--- internal/agents/agent.go
+++ internal/agents/agent.go
@@ -7,25 +7,27 @@ import (
 )
 
 type Agent struct {
-	ragService  *rag.RAGService
-	syncService *sync.SyncService
-	Repo        *repository.OpportunityRepository
+    ragService     *rag.RAGService
+    syncService    *sync.SyncService
+    browserService *browser.Service
+    repo           *repository.OpportunityRepository
+}
+func NewAgent(ragService *rag.RAGService, syncService *sync.SyncService,browserService *browser.Service,Repo *repository.OpportunityRepository) *Agent {
+	return &Agent{ragService: ragService, syncService: syncService,browserService: browserService, Repo: Repo}
 }
 
-func NewAgent(ragService *rag.RAGService, syncService *sync.SyncService, Repo *repository.OpportunityRepository) *Agent {
-	return &Agent{ragService: ragService, syncService: syncService, Repo: Repo}
-}
+//nodes of the Agent
 
+func (a *Agent) Route(ctx context.Context, state State) (State, error) {
+    ...
+}
 
 func (a *Agent) SyncGmail(ctx context.Context, state State) (State, error) {
     ...
 }
 
-func (a *Agent) ExtractOpportunities(ctx context.Context, state State) (State, error) {
-    ...
-}
 
-func (a *Agent) FindRelevantOpportunities(ctx context.Context, state State) (State, error) {
+func (a *Agent) RetrieveContext(ctx context.Context, state State) (State, error) {
     ...
 }
 
